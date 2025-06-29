e076d4e6b576a49e427bab6dadcf8b43f94c44e9
Muhammad Ardiansyah Asrifah_H071221068 <113808797+ArdiansyahAsrifah@users.noreply.github.com>
2025-06-29 15:02:25 +0700

    Initial Commit

---
+++ LumaTests/LumaTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumaTests.swift
+//  LumaTests
+//
+//  Created by Muhammad Ardiansyah Asrifah on 29/06/25.
+//
+
+import Testing
+@testable import Luma
+
+struct LumaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
